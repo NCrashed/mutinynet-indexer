1f838f4adb543c4b66b01fdec3c7b1b484a1e848
Anton Gushcha <ncrashed@protonmail.com>
2025-01-25 20:36:20 +0700

    Make the first test pass

--- vault-indexer/src/indexer/mod.rs
+++ vault-indexer/src/indexer/mod.rs
@@ -1,20 +1,27 @@
-use core::result::Result;
-use std::sync::mpmc::sync_channel;
+use core::{
+    result::Result,
+    sync::atomic::{self, AtomicBool},
+    time::Duration,
+};
+use event::{Event, EVENTS_CAPACITY};
 pub use network::Network;
-use event::EVENTS_CAPACITY;
+use std::sync::mpmc;
+use std::{sync::mpmc::sync_channel, thread};
 use thiserror::Error;
 
 use node::node_worker;
 
-mod node; 
 mod event;
 mod network;
+mod node;
 
 /// All kind of errors the indexer can produce
 #[derive(Debug, Error)]
 pub enum Error {
     #[error("Node worker failure: {0}")]
     Node(#[from] node::Error),
+    #[error("Failed to read from events bus, disconnected.")]
+    EventBusRecv,
 }
 
 /// The possible state of connection to bitcoin node we have.
@@ -33,6 +40,7 @@ pub struct Indexer {
     network: Network,
     node_address: String,
     start_height: u32,
+    node_connected: AtomicBool,
 }
 
 impl Indexer {
@@ -45,8 +53,14 @@ impl Indexer {
         self.network
     }
 
+    /// Get current state of connection the node
     pub fn node_status(&self) -> NodeStatus {
-        NodeStatus::Disconnected
+        let connected = self.node_connected.load(atomic::Ordering::Relaxed);
+        if connected {
+            NodeStatus::Connected
+        } else {
+            NodeStatus::Disconnected
+        }
     }
 
     /// Executes the internal threads (connection to the node, indexing worker) and awaits
@@ -54,7 +68,49 @@ impl Indexer {
     pub fn run(&self) -> Result<(), Error> {
         let (events_sender, events_receiver) = sync_channel(EVENTS_CAPACITY);
 
-        node_worker(&self.node_address, self.network, self.start_height, events_sender, events_receiver)?;
+        let node_handle = {
+            let address = self.node_address.clone();
+            let network = self.network;
+            let start_height = self.start_height;
+            let events_sender = events_sender.clone();
+            let events_receiver = events_receiver.clone();
+            thread::spawn(move || -> Result<(), Error> {
+                node_worker(
+                    &address,
+                    network,
+                    start_height,
+                    events_sender,
+                    events_receiver,
+                )?;
+                Ok(())
+            })
+        };
+
+        loop {
+            // Terminate if node worker ends with unrecoverable error
+            if node_handle.is_finished() {
+                let res = node_handle.join();
+                match res {
+                    Ok(Ok(_)) => break, // termination
+                    Ok(Err(e)) => return Err(e),
+                    Err(e) => std::panic::resume_unwind(e), // panic in the node worker
+                }
+            }
+
+            match events_receiver.recv_timeout(Duration::from_millis(100)) {
+                Err(mpmc::RecvTimeoutError::Timeout) => (), // take a chance to check termination
+                Err(mpmc::RecvTimeoutError::Disconnected) => return Err(Error::EventBusRecv),
+                Ok(Event::Handshaked) => {
+                    self.node_connected.store(true, atomic::Ordering::Relaxed);
+                }
+                Ok(Event::Disconnected) => {
+                    self.node_connected.store(false, atomic::Ordering::Relaxed);
+                }
+                Ok(Event::IncomingMessage(msg)) => {}
+                _ => (),
+            }
+        }
+
         Ok(())
     }
 }
@@ -97,6 +153,7 @@ impl IndexerBuilder {
             network: (self.network_builder)()?,
             node_address: (self.node_builder)()?,
             start_height: (self.start_height_builder)()?,
+            node_connected: AtomicBool::new(false),
         })
     }
 }
