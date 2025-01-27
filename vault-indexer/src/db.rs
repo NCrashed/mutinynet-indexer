b510effac714ceb330a603a2fe79de5613566a93
Anton Gushcha <ncrashed@protonmail.com>
2025-01-27 14:43:30 +0700

    Add metadata queries

--- vault-indexer/src/db.rs
+++ vault-indexer/src/db.rs
@@ -2,11 +2,11 @@ use bitcoin::{
     block::Header,
     consensus::{Decodable, Encodable},
     hashes::Hash,
-    network, BlockHash,
+    BlockHash,
 };
+use core::convert::TryInto;
 use log::*;
-use sqlite::{Connection, ReadableWithIndex, State, Statement, Value};
-use core::unimplemented;
+use sqlite::{Bindable, Connection, ReadableWithIndex, State, Statement, Value};
 use std::{io::Cursor, path::Path};
 use thiserror::Error;
 
@@ -34,6 +34,10 @@ pub enum Error {
     OrphanBlock(BlockHash, BlockHash),
     #[error("Query '{0}' should be executed by single step")]
     ShouldExecuteOneRow(String),
+    #[error("Failed to parse tip block hash (expected 32 bytes): {0:x?}")]
+    TipBlockHashWrongSize(Vec<u8>),
+    #[error("Database doesn't have a metadata row!")]
+    NoMetadata,
 }
 
 /// Wraps SQlite connection and caches prepared statements
@@ -46,6 +50,11 @@ pub struct HeaderRecord {
     pub header: Header,
     pub height: u32,
     pub in_longest: bool,
+}
+
+#[derive(Debug, Clone)]
+struct Metadata {
+    tip_block_hash: BlockHash,
 }
 
 impl Database {
@@ -75,7 +84,7 @@ impl Database {
 
             CREATE TABLE IF NOT EXISTS metadata (
                 id INTEGER PRIMARY KEY CHECK (id = 0), -- The table has only one row
-                tip_block_hash TEXT NOT NULL
+                tip_block_hash BLOB NOT NULL
             );
         ",
             )
@@ -86,6 +95,13 @@ impl Database {
         if self.load_block_header(genesis.block_hash())?.is_none() {
             let zero_hash = BlockHash::from_byte_array([0; 32]);
             self.store_raw_header(genesis, 0, zero_hash, true)?;
+        }
+
+        // Store initial metadata if missing
+        if !self.has_metadata()? {
+            self.store_metadata(&Metadata {
+                tip_block_hash: genesis.block_hash(),
+            })?;
         }
         Ok(())
     }
@@ -146,10 +162,6 @@ impl Database {
     ) -> Result<(), Error> {
         let query =
             "INSERT INTO headers VALUES(:block_hash, :height, :prev_block_hash, :raw, :in_longest)";
-        let mut statement = self
-            .connection
-            .prepare(query)
-            .map_err(Error::PrepareQuery)?;
 
         const HEADER_SIZE: usize = 80;
         let mut raw = vec![0u8; HEADER_SIZE];
@@ -157,8 +169,10 @@ impl Database {
             .consensus_encode(&mut Cursor::new(&mut raw))
             .map_err(Error::EncodeHeader)?;
 
-        statement
-            .bind_iter::<_, (_, Value)>([
+        self.single_execute::<_, (_, Value)>(
+            "insert header",
+            query,
+            [
                 (
                     ":block_hash",
                     header.block_hash().as_raw_hash().as_byte_array()[..].into(),
@@ -170,19 +184,85 @@ impl Database {
                 ),
                 (":raw", raw.into()),
                 (":in_longest", (if in_longest { 1 } else { 0 }).into()),
-            ])
-            .map_err(Error::BindQuery)?;
-
-        if let State::Done = statement.next().map_err(Error::QueryNextRow)? {
-            Ok(())
-        } else {
-            Err(Error::ShouldExecuteOneRow("insert header".to_owned()))
-        }
+            ],
+        )
     }
 
     /// Get current best main chain
     pub fn get_main_tip(&self) -> Result<BlockHash, Error> {
-        unimplemented!()
+        let meta = self.load_metada()?;
+        Ok(meta.tip_block_hash)
+    }
+
+    /// Returns true if we have single row in metadata table
+    fn has_metadata(&self) -> Result<bool, Error> {
+        let query = "SELECT count(id) as count FROM metadata";
+        let mut statement = self
+            .connection
+            .prepare(query)
+            .map_err(Error::PrepareQuery)?;
+
+        if let State::Row = statement.next().map_err(Error::QueryNextRow)? {
+            let count = statement.read_field::<i64>("count")?;
+            Ok(count != 0)
+        } else {
+            Ok(false)
+        }
+    }
+
+    // Update metadata table
+    fn store_metadata(&self, meta: &Metadata) -> Result<(), Error> {
+        let query = "INSERT INTO metadata VALUES(0, :tip_block_hash)
+                ON CONFLICT(id) DO UPDATE SET tip_block_hash=excluded.tip_block_hash";
+        self.single_execute::<_, (_, Value)>(
+            "upsert metadata",
+            query,
+            [(
+                ":tip_block_hash",
+                meta.tip_block_hash.as_raw_hash().as_byte_array()[..].into(),
+            )],
+        )
+    }
+
+    // Fetch all metadata from table
+    fn load_metada(&self) -> Result<Metadata, Error> {
+        let query = "SELECT * FROM metadata LIMIT 1";
+        let mut statement = self
+            .connection
+            .prepare(query)
+            .map_err(Error::PrepareQuery)?;
+
+        if let State::Row = statement.next().map_err(Error::QueryNextRow)? {
+            let tip_block_hash_bytes = statement.read_field::<Vec<u8>>("tip_block_hash")?;
+            let tip_block_hash_sized = tip_block_hash_bytes
+                .clone()
+                .try_into()
+                .map_err(|_| Error::TipBlockHashWrongSize(tip_block_hash_bytes))?;
+            let tip_block_hash = BlockHash::from_byte_array(tip_block_hash_sized);
+            Ok(Metadata { tip_block_hash })
+        } else {
+            Err(Error::NoMetadata)
+        }
+    }
+
+    // Helper to execute inserts, updates and deletes (etc) that should be done in one step with no return results
+    fn single_execute<T, U>(&self, tag: &str, query: &str, binds: T) -> Result<(), Error>
+    where
+        T: IntoIterator<Item = U>,
+        U: Bindable,
+    {
+        let mut statement = self
+            .connection
+            .prepare(query)
+            .map_err(Error::PrepareQuery)?;
+
+        statement.bind_iter(binds).map_err(Error::BindQuery)?;
+
+        if let State::Done = statement.next().map_err(Error::QueryNextRow)? {
+            Ok(())
+        } else {
+            Err(Error::ShouldExecuteOneRow(tag.to_owned()))
+        }
     }
 }
 
